6eb7581dcaa4fdce2d4eb53308c8145894148b1e
unknown <lhcode_web@163.com>
2020-06-24 08:43:26 +0800

    tijiaodaima

--- src/network/index.js
+++ src/network/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function request(config) {
   const instance = axios.create({
-    baseURL:'xxxxxxxxxxxxxxxxx',
+    baseURL: 'http://123.207.32.32:8000/api/m3',
     timeout: 5000
   })
   return instance(config);
