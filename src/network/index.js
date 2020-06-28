f02ab972ebeb4504abd71e867e089540e2c6edef
lhcoder-web <67143423+lhcoder-web@users.noreply.github.com>
2020-06-28 09:52:23 +0800

    Update index.js

--- src/network/index.js
+++ src/network/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://123.207.32.32:8000/api/m3',
+    baseURL: 'xxxxxxxxxxxxxxx',
     timeout: 5000
   })
   return instance(config);
